262a61903c8274d7a698bb06507416a18bf35877
Nico <nico.bsh13@gmail.com>
2023-11-05 22:01:33 -0500

    Se realizan migraciones para añadir modelos de base de datos para los anuncios, así como se agregan columnas de grupo y estado a la tabla usuario, y se reordenan las carpettas de los componentes para una mejor comprensión del código

--- modelos.py
+++ modelos.py
@@ -10,6 +10,12 @@ class Rol(db.Model):
     rol = db.Column(db.String(50), unique=True, nullable=False)
 
 # Definición de la tabla "usuarios"
+class Grupos(db.Model):
+    __tablename__ = 'grupos'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    grupo = db.Column(db.String(20), unique = True)
+
 class Usuario(db.Model, UserMixin):
     __tablename__ = 'usuarios'
 
@@ -21,12 +27,34 @@ class Usuario(db.Model, UserMixin):
     email = db.Column(db.String(80), unique=True, nullable=False)
     tel = db.Column(db.String(10), nullable=False)
     rol_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+    grupo_id = db.Column(db.Integer, db.ForeignKey('grupos.id'), nullable=True)
     contrasena = db.Column(db.String(255), nullable=False)
     fecha_registro = db.Column(db.DateTime, default = datetime.utcnow)
     fecha_actualizado = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    estado = db.Column(db.String(20), nullable=False, default='activo')
 
     # Relación con la tabla "roles"
+    grupo = db.relationship('Grupos', foreign_keys = [grupo_id])
     rol = db.relationship('Rol')
+
+    
+
+class Posts(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column(db.String(30), nullable = False)
+    texto = db.Column(db.Text, nullable = False)
+    autor_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    fecha_publicado = db.Column(db.DateTime, default = datetime.utcnow)
+    fecha_editado = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    destino_id = db.Column(db.Integer, db.ForeignKey('grupos.id'), nullable=True)
+    visibilidad = db.Column(db.String(30), default = "visible")
+
+    
+    # Relación con la tabla "usuarios" para acceder al autor
+    autor = db.relationship('Usuario', foreign_keys=[autor_id])
+    destino = db.relationship('Grupos', foreign_keys=[destino_id])
 
 usuarios = Usuario.query.order_by(Usuario.apellido)
 
